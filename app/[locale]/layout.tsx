40abe2af4aa2c36074673966101e44825e28b597
Jose Lorenzo <jose.lorenzo117@alu.ulpgc.es>
2026-02-01 15:54:32 +0000

    Cambios

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "EduRinconAI",
+    title: "MentorAI",
     description: "Sistema de asistentes educativos con IA",
 };
 
